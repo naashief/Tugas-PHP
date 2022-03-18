f0ca21ed2d858e2031d86b6dfce795c99486cf28
naashief <42881248+naashief@users.noreply.github.com>
2022-03-19 04:29:29 +0700

    Add files via upload

---
+++ praktikum-3/layout/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>SAS-STTNF</title>
+</head>
+
+Web Programming | STT-NF
+ 5
+Praktikum 3 : PHP Dasar
+<body>
+
+<header role="banner">
+<h2>Student Activity Score - STTNF</h2>
+<a href="#">Home</a> | <a href="#">Activity</a> | <a href="#">My Score</a>
+| <a href="#">Login</a>
+<hr/>
+</header>
